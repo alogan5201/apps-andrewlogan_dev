c82f78f2109be2672840917e6881bbc5de70bd1a
alogan5201 <andrewthomaslogan@gmail.com>
2022-05-06 22:12:52 -0400

    up

--- apps/rental_app/service-worker.js
+++ apps/rental_app/service-worker.js
@@ -7,11 +7,11 @@
 
 self.addEventListener('install', function(event) {
   event.waitUntil(preLoad());
-  console.log('Thorium  Builder Service Worker Iniialized');
+  
 });
 
 var preLoad = function(){
-  console.log('Thorium  Builder Service Worker Installation');
+  
   return caches.open('thorium-cache').then(function(cache) {
     return cache.addAll(['./','./offline.html', './index.html', './manifest.webmanifest']);
   });
@@ -19,7 +19,7 @@ var preLoad = function(){
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(checkResponse(event.request).catch(function() {
-    console.log('Thorium  Builder Service Worker returned from cache : '+JSON.stringify(event.request,null,2));
+    
     return returnFromCache(event.request)}
   ));
   event.waitUntil(addToCache(event.request));
@@ -28,7 +28,7 @@ self.addEventListener('fetch', function(event) {
 var checkResponse = function(request){
   return new Promise(function(fulfill, reject) {
     fetch(request).then(function(response){
-      console.log('Thorium  Builder Service Worker Response Status '+response.status+": "+response.url);
+      
       if(response.status !== 404) {
         fulfill(response)
       } else {
@@ -41,7 +41,7 @@ var checkResponse = function(request){
 var addToCache = function(request){
   return caches.open('thorium-cache').then(function (cache) {
     return fetch(request).then(function (response) {
-      console.log('Thorium  Builder Service Worker Add to Cache '+response.url);
+      
       return cache.put(request, response);
     });
   });
@@ -52,14 +52,14 @@ var returnFromCache = function(request){
     return cache.match(request).then(function (matching) {
      if(!matching || matching.status == 404) {
       if ((request)  && (request.url.indexOf('.php')!= -1)) { 
-        console.log("service Worker returns network error");
+        
         throw new TypeError('network-error');
       } else {
-        console.log("service Worker returns offline page");
+        
         return cache.match('offline.html');
       }
      } else {
-      console.log("service Worker returns cached request");
+      
        return matching
      }
     });
